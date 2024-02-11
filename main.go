0ce1cd57272f960b542aaabbaed5c4fb693b4d2c
Meowzmo <dget2013@gmail.com>
2024-02-11 11:39:04 +0300

    block duels

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ const (
 	MeasureCommand      = "/check_size"
 	AverageCommand      = "/get_average"
 	TodayCommand        = "/last_measures"
-	DuelCommand         = "/duel"
+	DuelCommand         = "/duel42434556456456e"
 	DuelStatsCommand    = "/stats"
 
 	WORKERS_PUll = 10
